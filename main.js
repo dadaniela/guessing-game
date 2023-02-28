d99aa91a1f69632a5429f7798a03ea817490cae2
Daniela <trindadedv@gmail.com>
2023-02-28 19:24:25 +0100

    JS changes to make it clearer, new colors

--- main.js
+++ main.js
@@ -1,29 +1,37 @@
-const randomNum = Math.round(Math.random() * 10);
-console.log(randomNum);
-
+const btnGuess = document.querySelector("#btn-guess");
+const btnReset = document.querySelector("#btn-reset");
+let randomNum = Math.round(Math.random() * 10);
 let attempts = 1;
 
+btnGuess.addEventListener("click", handleGuessClick);
+btnReset.addEventListener("click", handleResetClick);
+console.log(randomNum)
 function handleGuessClick(event) {
     event.preventDefault();
 
     const inputNumber = document.querySelector("#inputNumber");
 
     if(Number(inputNumber.value) == randomNum) {
-        document.querySelector(".page1").classList.add("hide");
-        document.querySelector(".page2").classList.remove("hide");
+        togglePage();
         const attemptsMsg = attempts > 1 ? "attempts" : "attempt";
         document.querySelector(".page2 h2").innerText = `You got it right in ${attempts} ${attemptsMsg}`;        
     }
     inputNumber.value = "";
     attempts++;
+};
+
+function handleResetClick() {
+    togglePage();
+    attempts = 1;
+    // randomNum = Math.round(Math.random() * 10);
+};
+
+function togglePage() {
+    document.querySelector(".page1").classList.toggle("hide");
+    document.querySelector(".page2").classList.toggle("hide");
 }
 
-const btnGuess = document.querySelector("#btn-guess");
-const btnReset = document.querySelector("#btn-reset");
-
-btnGuess.addEventListener("click", handleGuessClick);
-btnReset.addEventListener("click", function() {
-    document.querySelector(".page2").classList.add("hide");
-    document.querySelector(".page1").classList.remove("hide");
-    attempts = 1;
-})
+// todo:
+// add a limit to accept only nums btw 0 and 10
+// keep the btnGuess from counting the click when there is no input
+// bug - randomNum not resetting
